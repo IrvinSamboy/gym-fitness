8a7fdd05d542e97860beb694e031db9d1daf1949
unknown <contacoirvinsamboy@gmail.com>
2025-03-15 16:04:05 -0400

    showing the posts

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-<h1>Hello wordpress</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <?php
+            while(have_posts()) : 
+                
+                the_post();
+
+                the_title();
+
+                the_content();
+            endwhile;
+        ?>
+    </main>
+</body>
+</html>
