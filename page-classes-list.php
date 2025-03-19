e7f95cbb11f6255a41a01cf4747d5e07cc637d33
unknown <contacoirvinsamboy@gmail.com>
2025-03-18 23:01:03 -0400

    Showing title of post

--- page-classes-list.php
+++ page-classes-list.php
@@ -6,7 +6,22 @@
 ?>
 
     <main class="container section">
-      
+      <ul>
+        <?php
+            $args = array(
+                'post_type' => 'gymfitness_clases'
+            );
+
+            $classes = new WP_Query($args);
+            
+            while($classes->have_posts()){
+                $classes->the_post();
+            ?>
+                <li><?php the_title() ?></li>
+            <?php
+            }
+        ?>
+      </ul>
     </main>
 
 <?php
